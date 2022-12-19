690d21615d657b7cf6f6aafa0d65bac9d56c3a7a
Caio César Oliveira <54439337+oCaioOliveira@users.noreply.github.com>
2022-12-19 11:43:38 -0300

    :rocket: Adição da rota item

--- src/Routes.tsx
+++ src/Routes.tsx
@@ -2,22 +2,24 @@ import {
     BrowserRouter as Router,
     Routes,
     Route
-  } from "react-router-dom";
-  
-  import { PaginaInicial } from "./pages/PaginaInicial";
-  import { Cardapio } from "./pages/Cardapio";
-  import { Login } from "./pages/Login";
-  import { GerenciamentoMesas } from "./pages/GerenciamentoMesas";
-  
-  export function AppRoutes() {
-      return (
-          <Router>
-              <Routes>
-                  <Route path="/paginaInicial" element={<PaginaInicial />} />
-                  <Route path="/cardapio" element={<Cardapio />} />
-                  <Route path="/login" element={<Login />} />
-                  <Route path="/gerenciamentomesas" element={<GerenciamentoMesas />} />
-              </Routes>
-          </Router>
-      )
-  }
+} from "react-router-dom";
+
+import { PaginaInicial } from "./pages/PaginaInicial";
+import { Cardapio } from "./pages/Cardapio";
+import { Login } from "./pages/Login";
+import { GerenciamentoMesas } from "./pages/GerenciamentoMesas";
+import { Item } from "./pages/Item";
+
+export function AppRoutes() {
+    return (
+        <Router>
+            <Routes>
+                <Route path="/paginaInicial" element={<PaginaInicial />} />
+                <Route path="/cardapio" element={<Cardapio />} />
+                <Route path="/login" element={<Login />} />
+                <Route path="/gerenciamentomesas" element={<GerenciamentoMesas />} />
+                <Route path="/item" element={<Item />} />
+            </Routes>
+        </Router>
+    )
+}
