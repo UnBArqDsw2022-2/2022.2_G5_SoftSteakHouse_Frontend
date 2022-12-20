a1e592bbcc49d56c76ebb24d687acb79a4cb8536
oCaioOliveira <caiocesar31951910@hotmail.com>
2022-12-20 13:10:46 -0300

    :rocket: Adição de responsividade ao sistema

--- src/pages/Item.tsx
+++ src/pages/Item.tsx
@@ -9,16 +9,13 @@ export function Item() {
             <h1>Batata Frita</h1>
             <p>Batata colhida nos lindos campos de Barba Longa e frita no óleo usado nos pastéis
                 do dia anterior. Temperada com a grama triturada do comércio vizinho.</p>
+            <br />
             <p>R$ 19,99</p>
             <hr />
             <h2>Adicionais</h2>
-            <button name="Botão Adicionais">Refri Lata _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ R$ 08,99</button>
-            <br />
-            <br />
-            <button name="Botão Adicionais">Cheddar e Bacon _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ R$ 08,99</button>
-            <br />
-            <br />
-            <button name="Botão Adicionais">Filé e Provolone _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ R$ 11,99</button>
+            <button name="Botão Adicionais">Refri Lata R$ 08,99</button>
+            <button name="Botão Adicionais">Cheddar e Bacon  R$ 08,99</button>
+            <button name="Botão Adicionais">Filé e Provolone R$ 11,99</button>
         </div >
     );
 }
