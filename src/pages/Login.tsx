5f190a6def3e35d59e9f4df51b7127fc2632f11d
Marcos Felipe <u00334@brbseguros.com.br>
2022-12-28 20:06:27 -0300

    <feat>[Login]: Esqueleto da pagina de login
    co-authored: Taynara taynaracris11@hotmail.com

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,3 +1,50 @@
+import { useState } from "react";
+import "./Login.css";
+import { User } from 'react-feather';
+
 export function Login() {
-    return <h1>Login</h1>
+    const [user, setUser] = useState({
+        email: "",
+        password: ""
+    });
+
+function onChangePassword(password: string) {
+    if(password.length > 8) return;
+    setUser({...user, password: password});
+}
+
+function onChangeEmail(email: string) {
+    if(email.length > 8) return;
+    setUser({...user, password: email});
+}
+
+
+    function onSubmit() {
+         console.log('ok');
+    }
+    return (
+        <body>
+            <form onSubmit={onSubmit}>
+                <section>
+                    <User color="white" size={150} stroke-width="1" />
+                </section>               
+                <section>
+                    <div className="input-container">
+                        <input placeholder="Usuário" onChange={(e) => onChangeEmail(e.target.value)}/>
+                    </div>
+                    <div className="input-container">
+                        <input type="password" placeholder="Senha" onChange={(e) => onChangePassword(e.target.value)}/>
+                    </div>
+                </section>
+                <section>
+                    <div className="submit-div">
+                        <button type="submit" className="submit-button"><span>Entrar</span></button>
+                    </div>
+                    <div className="submit-div">
+                        <a href="#">Esqueceu a senha?</a>
+                    </div>
+                </section>
+            </form>
+        </body>
+    );
 }
