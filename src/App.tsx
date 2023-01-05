ff0f0f6bca6dab5fe9b1710caec20810ff8137ac
Carlacangussu <40703846+Carlacangussu@users.noreply.github.com>
2023-01-05 20:56:45 -0300

    Versão original

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,9 @@
 import './App.css';
 import { AppRoutes } from "./Routes";
-import { PaginaInicial } from './pages/PaginaInicial';
-import { Cardapio } from './pages/Cardapio';
 
 function App() {
   return (
-    <Cardapio />
+    <AppRoutes />
   );
 }
 
