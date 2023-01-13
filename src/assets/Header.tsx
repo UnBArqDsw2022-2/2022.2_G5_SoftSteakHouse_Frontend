aac2a4d056bfe2ef5e40ded0daff97ca76f60944
oCaioOliveira <caiocesar31951910@hotmail.com>
2023-01-13 16:14:33 -0300

    :rocket: Adição do sidebar ao Header

--- src/assets/Header.tsx
+++ src/assets/Header.tsx
@@ -1,19 +1,30 @@
-import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
-import {faBars} from '@fortawesome/free-solid-svg-icons';
-import {faUser} from '@fortawesome/free-solid-svg-icons'; 
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faBars } from '@fortawesome/free-solid-svg-icons';
+import { faUser } from '@fortawesome/free-solid-svg-icons';
 import '../pages/PaginaInicial.css';
+import '../pages/Sidebar'
+import { Sidebar } from '../pages/Sidebar';
+import { useState } from 'react';
 
 export const Header = () => {
+    const [showSidebar, setShowSidebar] = useState(false);
+
+    function handleClick() {
+        setShowSidebar(!showSidebar);
+    }
+
     return (
         <header>
-            <span>
-            <FontAwesomeIcon className="menu" icon={faBars} />
-            <FontAwesomeIcon className="singIn" icon={faUser}/>
+            <span onClick={handleClick}>
+                <FontAwesomeIcon className="menu" icon={faBars} />
             </span>
-            <br/>
-            <hr/>
-            <br/>
+            <span>
+                <FontAwesomeIcon className="singIn" icon={faUser} />
+            </span>
+            <br />
+            {showSidebar && <Sidebar />}
+            <hr />
+            <br />
         </header>
-
     );
 }
