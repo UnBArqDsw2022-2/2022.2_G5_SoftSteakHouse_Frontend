6b55a57fe99fc7c932dec87b462b3ed49b3c0141
Marcos Felipe <u00334@brbseguros.com.br>
2023-01-28 19:41:06 -0300

    :rocket: Criação da tela de gerenciamento de mesas

--- src/pages/GerenciamentoMesas.tsx
+++ src/pages/GerenciamentoMesas.tsx
@@ -1,3 +1,78 @@
+import { useEffect, useState } from "react";
+import { Plus, Minus, Info } from "react-feather";
+import { Header } from "../assets/Header";
+import { api, useFetch, useMutate } from "../hooks/useFetch";
+import "./GerenciamentoMesas.css";
+
+interface Mesa {
+    id: number,
+    nomeTipo: string,
+    qtdCadeiras: number,
+    qtdMesas: number
+}
+
 export function GerenciamentoMesas() {
-    return <h1>Gerenciamento de Mesas</h1>
+    const { data, error, isFetching } = useFetch<Mesa[]>('api/v1/mesas');
+    const[mesas, setMesas] = useState<Mesa[] | null>(data)
+    
+    useEffect(() => {
+        if(!isFetching)
+        setMesas(data)
+    }, [isFetching])
+
+    const update = (mesa:Mesa) => {
+        if(!mesa.id) return;
+        api.put<Mesa>(`api/v1/mesas/${mesa.id}`, mesa)
+            .then((result) => {
+                setMesas((prev) => {
+                    const tmp = [...prev] ?? [];
+                    const obj = tmp.find(({id}) => id == mesa.id);
+                    const index = tmp.indexOf(obj);
+                    tmp[index] = result.data;
+                    return tmp;
+                })
+            });
+    }
+
+    function renderSelectOptions(mesa: Mesa) {
+        var rows = [];
+        for(var i = 1;i <= mesa.qtdMesas;i++)
+            rows.push(i);
+        return rows.map((i) => <option className="PaginaMesasSelectOptions" selected={mesa.qtdMesas === i} value={i}>{i}</option>);
+    }
+
+    function renderTipoMesa(tipo:Mesa) {
+        function add() { update({...tipo, qtdMesas: tipo.qtdMesas + 1}) }
+        function remove() { update({...tipo, qtdMesas: tipo.qtdMesas - 1}) }
+        return(
+            <div className="row" key={tipo}>
+                
+                <label className="label">{`Mesa para ${tipo.nomeTipo}`}: </label>
+                <select  onChange={({target: {value}}) => update({...tipo, qtdMesas: parseInt(value)})} className="select" placeholder="Escolha">
+                    {renderSelectOptions(tipo)}
+                </select>
+                <div className="buttons-row">
+                    <button className="PaginaMesasIconButton plus" onClick={add}>
+                        <Plus color="#ffffff"strokeWidth={5}/>
+                    </button>
+                    <button className="PaginaMesasIconButton minus"onClick={remove}>
+                        <Minus color="#000000" strokeWidth={5}/>
+                    </button>
+                </div>
+            </div>
+        )
+    }
+
+    return (
+        <div className="PaginaMesasCorpo">  
+            <Header />  
+            <div className="PaginaMesasEstrutura">
+                <hr className="PaginaMesasBarra" />  
+                <h3 className="title">Gerenciamento de Mesas <Info /> </h3>
+                <section className="PaginaMesasItem">
+                    {mesas && mesas.map(renderTipoMesa)}
+                </section>
+            </div>
+        </div>
+    )
 }
