b2721288b1f6ec3ed80c5aafabcd876c8a8efac7
Marcos Felipe <u00334@brbseguros.com.br>
2023-01-28 19:52:12 -0300

    :rocket: Ajuste do css da pagina de genreciamento

--- src/pages/GerenciamentoMesas.tsx
+++ src/pages/GerenciamentoMesas.tsx
@@ -1,7 +1,7 @@
 import { useEffect, useState } from "react";
 import { Plus, Minus, Info } from "react-feather";
 import { Header } from "../assets/Header";
-import { api, useFetch, useMutate } from "../hooks/useFetch";
+import { api, useFetch } from "../hooks/useFetch";
 import "./GerenciamentoMesas.css";
 
 interface Mesa {
@@ -38,14 +38,14 @@ export function GerenciamentoMesas() {
         var rows = [];
         for(var i = 1;i <= mesa.qtdMesas;i++)
             rows.push(i);
-        return rows.map((i) => <option className="PaginaMesasSelectOptions" selected={mesa.qtdMesas === i} value={i}>{i}</option>);
+        return rows.map((i) => <option key={i} className="PaginaMesasSelectOptions" selected={mesa.qtdMesas === i} value={i}>{i}</option>);
     }
 
     function renderTipoMesa(tipo:Mesa) {
         function add() { update({...tipo, qtdMesas: tipo.qtdMesas + 1}) }
         function remove() { update({...tipo, qtdMesas: tipo.qtdMesas - 1}) }
         return(
-            <div className="row" key={tipo}>
+            <div className="PaginaMesasLinha" key={tipo.id}>
                 
                 <label className="label">{`Mesa para ${tipo.nomeTipo}`}: </label>
                 <select  onChange={({target: {value}}) => update({...tipo, qtdMesas: parseInt(value)})} className="select" placeholder="Escolha">
